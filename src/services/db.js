38500be3027a26507d6a558b8b1e05d2e75c3699
WellRK <65877834+WellRK@users.noreply.github.com>
2021-11-14 11:55:05 -0300

    Update DB

--- src/services/db.js
+++ src/services/db.js
@@ -42,10 +42,27 @@ async function connect() {
   return _db;
 }
 
-connect();
+const connect1 =  async ()=>{
+
+  try{
+      const conn = await mongoose.connect(process.env.MONGO_URL,{
+          //must add in order to not get any error masseges:
+          useUnifiedTopology:true,
+          useNewUrlParser: true
+         
+      })
+      console.log(`mongo database is connected!!! ${conn.connection.host} `)
+  }catch(error){
+      console.error(`Error: ${error} `)
+      process.exit(1) //passing 1 - will exit the proccess with error
+  }
+
+}
+
+connect1();
 
 function getConnection() {
   return _db;
 }
 
-export { connect, getConnection };
+export { connect1, getConnection };
